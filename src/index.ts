e21985f1404d0c89bb91baf7d76c36c4b3276aad
Nalosa1987 <104040075+Nalosa1987@users.noreply.github.com>
2022-04-26 19:38:17 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+//ejercicio 1a.
+//chocotorta.
+
+//declaracion de variables:
+let queso: number = 400;
+let ddl: number = 400;
+let azucar: number = 250;
+let crema: number = 250;
+let galletitas: number = 1000;
+let relleno: number = 1300;
+
+//para el relleno:
+
+console.log(queso + 400);
+console.log("mezclar el queso con el ddl");
+console.log("agregar " + "crema " + 250);
+console.log("seguir batiendo");
+console.log("agregar " + "azucar " + 250);
+console.log("mezclar hasta incorporar los ingredientes";
+
+//para el armado de la torta.
+
+console.log("en una placa rectangular");
+console.log(galletitas + 1300);
+console.log("cubrir la placa con las galletitas e incorporcar el relleno");
+console.log("repetir esto hasta cubrir la placa")
+
+
+
+
+
+
+
+
